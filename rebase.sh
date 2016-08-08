595b112d9ba56ed59933194c9f51c87fc5c15990
steve allen <sallen@amberstyle.ca>
2016-08-08 14:58:50 -0700

    Updated rebase to log to syslog

--- rebase.sh
+++ rebase.sh
@@ -7,15 +7,19 @@ REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @ @{u})
 
 if [ $LOCAL = $BASE ]; then
-  echo "Rebasing"
+  logger System "Updating authorized_keys (git commit $BASE)"
   git fetch
   git reset --hard origin/master
-  echo "Restarting SSH (a kludge to kick expired key sessions)"
+  logger System "Kicking active SSH sessions"
+
+  SSH_SERVICE=sshd
+
+  if [ -f /etc/debian_version ]; then
+    SSH_SERVICE=ssh
+  fi
 
   sudo pkill --signal HUP sshd
-
-else
-  echo "Up-to-date, nothing to do"
+  sudo service $SSH_SERVICE restart
 fi
 
 chmod 644 authorized_keys
