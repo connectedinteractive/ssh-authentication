b9c405c1e35c2cb1f0a5d4e7780471ea3e049935
steve allen <sallen@amberstyle.ca>
2016-08-08 14:44:59 -0700

    Bouncing SSH now terminates active connections

--- rebase.sh
+++ rebase.sh
@@ -12,11 +12,15 @@ if [ $LOCAL = $BASE ]; then
   git reset --hard origin/master
   echo "Restarting SSH (a kludge to kick expired key sessions)"
 
+
+  SSH_SERVICE=sshd
+
   if [ -f /etc/debian_version ]; then
-    sudo service ssh restart
-  elif [ -f /etc/redhat-release ]; then
-    sudo service sshd restart
+    SSH_SERVICE=ssh
   fi
+
+  sudo service $SSH_SERVICE stop
+  sudo service $SSH_SERVICE start
 
 else
   echo "Up-to-date, nothing to do"
